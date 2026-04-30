af6d0baed8c2b1a5f1ff714021ce3191ad07ee6f
유지수 <jsyu333@gmail.com>
2026-05-01 05:07:00 +0900

    fix: ocr.js 경로 변경

--- routes/scan.js
+++ routes/scan.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { extractTextFromImage } = require('../utils/ocr');
+const { extractTextFromImage } = require('../models/ocr');
 
 const upload = multer({ storage: multer.memoryStorage() });
 
